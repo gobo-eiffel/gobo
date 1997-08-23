bf559aa5f9b05f5fbdce34bd4b80991c68548e8a
Eric Bezault <ericb@gobosoft.com>
1997-08-23 18:06:39 +0000

    Changed cluster pathname. New version of SmallEiffel: -0.84
    git-svn-id: file:///c/gobo/svn/gobo/trunk@151 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- example/lexical/ascii2ps/se-0.83.bat
+++ example/lexical/ascii2ps/se-0.83.bat
@@ -1,5 +1,5 @@
 @rem system:     "'ascii2ps' pretty-printer"
-@rem compiler:   "SmallEiffel -0.85"
+@rem compiler:   "SmallEiffel -0.84"
 @rem author:     "Eric Bezault <ericb@gobo.demon.co.uk>"
 @rem copyright:  "Copyright (c) 1997, Eric Bezault"
 @rem date:       "$Date$"
@@ -7,7 +7,7 @@
 
 
 @echo .\>									loadpath.se
-@echo %GOBO%\example\ascii2ps\>>			loadpath.se
+@echo %GOBO%\example\lexical\ascii2ps\>>	loadpath.se
 
 @rem	-- Gobo Eiffel Lexical Library
 @echo %GOBO%\library\lexical\skeleton\>>	loadpath.se
@@ -17,7 +17,7 @@
 @echo %GOBO%\library\kernel\spec\se\>>		loadpath.se
 
 @rem	-- SmallEiffel Kernel
-@echo %SmallEiffel%\std_lib\>>				loadpath.se
+@echo %SmallEiffel%\lib_std\>>				loadpath.se
 
 
 compile_to_c -boost -no_split ASCII2PS make
