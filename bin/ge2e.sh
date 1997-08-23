123ad706b02b0c747ca26226df51e390389edca2
Eric Bezault <ericb@gobosoft.com>
1997-08-23 17:58:39 +0000

    Added support for TowerEiffel.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@146 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- bin/ge2e.sh
+++ bin/ge2e.sh
@@ -4,7 +4,7 @@
 
 # Preprocess '<filename>.ge' files in directory 'dirname'.
 # Write output to corresponding '<filename>.e' files in
-# directory 'dirname'/spec/(ise|ve|se|hact)/<filename>.e.
+# directory 'dirname'/spec/(ise|ve|se|hact|tower)/<filename>.e.
 
 cd $1
 
@@ -18,10 +18,12 @@ if [ -d spec/ve/ecluster ]; then cd spec/ve ; vec /dc /y ; cd ../.. ; fi
 rm -f spec/ve/*.e
 if [ ! -d spec/se ]; then mkdir spec/se ; fi
 rm -f spec/se/*.e
+if [ ! -d spec/tower ]; then mkdir spec/tower ; fi
+rm -f spec/tower/*.e
 
 for file in `ls *.ge` ; do
 	echo $file
-	for compiler in ISE HACT VE SE ; do
+	for compiler in ISE HACT VE SE TOWER ; do
 #		touch spec/$compiler/`basename $file .ge`.e
 		gepp -D$compiler $file spec/$compiler/`basename $file .ge`.e
 	done
