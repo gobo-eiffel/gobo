997c16214fd34780fcf555d81e81e3973c769a42
Eric Bezault <ericb@gobosoft.com>
1999-03-29 18:15:58 +0000

    New SmallEiffel release: -0.78 Use -no_style_warning instead of -no_warning. Use new environment variable format: ${GOBO}
    git-svn-id: file:///c/gobo/svn/gobo/trunk@618 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- src/gepp/se.bat
+++ src/gepp/se.bat
@@ -1,42 +1,40 @@
 @rem system:     "'gepp' preprocessor"
-@rem compiler:   "SmallEiffel -0.80"
+@rem compiler:   "SmallEiffel -0.78"
 @rem author:     "Eric Bezault <ericb@gobo.demon.co.uk>"
-@rem copyright:  "Copyright (c) 1998, Eric Bezault"
+@rem copyright:  "Copyright (c) 1999, Eric Bezault"
 @rem date:       "$Date$"
 @rem revision:   "$Revision$"
 
 
-@echo .\>											loadpath.se
-@echo %GOBO%\src\gepp\>>							loadpath.se
+@echo ${GOBO}\src\gepp\>							loadpath.se
 
 @rem	-- Gobo Eiffel Lexical Library
-@echo %GOBO%\library\lexical\skeleton\>>			loadpath.se
+@echo ${GOBO}\library\lexical\skeleton\>>			loadpath.se
 
 @rem	-- Gobo Eiffel Parse Library
-@echo %GOBO%\library\parse\skeleton\>>				loadpath.se
+@echo ${GOBO}\library\parse\skeleton\>>				loadpath.se
 
 @rem	-- Gobo Eiffel Utility Library
-@echo %GOBO%\library\utility\error\>>				loadpath.se
-@echo %GOBO%\library\utility\support\>>				loadpath.se
+@echo ${GOBO}\library\utility\error\>>				loadpath.se
+@echo ${GOBO}\library\utility\support\>>			loadpath.se
 
 @rem	-- Gobo Eiffel Kernel Library
-@echo %GOBO%\library\kernel\>>						loadpath.se
-@echo %GOBO%\library\kernel\spec\se\>>				loadpath.se
+@echo ${GOBO}\library\kernel\>>						loadpath.se
+@echo ${GOBO}\library\kernel\spec\se\>>				loadpath.se
 
 @rem	-- Gobo Eiffel Structure Library
-@echo %GOBO%\library\structure\container\>>			loadpath.se
-@echo %GOBO%\library\structure\cursor\>>			loadpath.se
-@echo %GOBO%\library\structure\list\>>				loadpath.se
-@echo %GOBO%\library\structure\search\>>			loadpath.se
-@echo %GOBO%\library\structure\sort\>>				loadpath.se
-@echo %GOBO%\library\structure\support\>>			loadpath.se
-@echo %GOBO%\library\structure\stack\>>				loadpath.se
-@echo %GOBO%\library\structure\table\>>				loadpath.se
+@echo ${GOBO}\library\structure\container\>>		loadpath.se
+@echo ${GOBO}\library\structure\cursor\>>			loadpath.se
+@echo ${GOBO}\library\structure\list\>>				loadpath.se
+@echo ${GOBO}\library\structure\search\>>			loadpath.se
+@echo ${GOBO}\library\structure\sort\>>				loadpath.se
+@echo ${GOBO}\library\structure\support\>>			loadpath.se
+@echo ${GOBO}\library\structure\stack\>>			loadpath.se
+@echo ${GOBO}\library\structure\table\>>			loadpath.se
 
 @rem	-- SmallEiffel Kernel
-@echo %SmallEiffel%\lib_std\>>						loadpath.se
+@rem ${SmallEiffelDir}\lib_std\>>					loadpath.se
 
 
-set options= -boost -no_split -no_warning -no_gc
-compile_to_c %options% GEPP execute
-gepp.bat
+set options= -boost -no_split -no_style_warning -no_gc
+compile %options% GEPP execute
