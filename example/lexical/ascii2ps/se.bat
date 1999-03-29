5b16a6100e072c6aac930bf69494b1013342383a
Eric Bezault <ericb@gobosoft.com>
1999-03-29 17:31:33 +0000

    New SmallEiffel release: -0.78 Use -no_style_warning instead of -no_warning. Use new environment variable format: ${GOBO}
    git-svn-id: file:///c/gobo/svn/gobo/trunk@561 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- example/lexical/ascii2ps/se.bat
+++ example/lexical/ascii2ps/se.bat
@@ -1,25 +1,23 @@
 @rem system:     "'ascii2ps' pretty-printer"
-@rem compiler:   "SmallEiffel -0.80"
+@rem compiler:   "SmallEiffel -0.78"
 @rem author:     "Eric Bezault <ericb@gobo.demon.co.uk>"
 @rem copyright:  "Copyright (c) 1998, Eric Bezault"
 @rem date:       "$Date$"
 @rem revision:   "$Revision$"
 
 
-@echo .\>									loadpath.se
-@echo %GOBO%\example\lexical\ascii2ps\>>	loadpath.se
+@echo ${GOBO}\example\lexical\ascii2ps\>	loadpath.se
 
 @rem	-- Gobo Eiffel Lexical Library
-@echo %GOBO%\library\lexical\skeleton\>>	loadpath.se
+@echo ${GOBO}\library\lexical\skeleton\>>	loadpath.se
 
 @rem	-- Gobo Eiffel Kernel Library
-@echo %GOBO%\library\kernel\>>				loadpath.se
-@echo %GOBO%\library\kernel\spec\se\>>		loadpath.se
+@echo ${GOBO}\library\kernel\>>				loadpath.se
+@echo ${GOBO}\library\kernel\spec\se\>>		loadpath.se
 
 @rem	-- SmallEiffel Kernel
-@echo %SmallEiffel%\lib_std\>>				loadpath.se
+@rem ${SmallEiffelDir}\lib_std\>>			loadpath.se
 
 
-set options= -boost -no_split -no_warning -no_gc
-compile_to_c %options% ASCII2PS make
-ascii2ps.bat
+set options= -boost -no_split -no_style_warning -no_gc
+compile %options% ASCII2PS make
