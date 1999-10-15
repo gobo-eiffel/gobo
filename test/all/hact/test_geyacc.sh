fe704e9c13c8be0f87d68959c89fb7e922847297
Eric Bezault <ericb@gobosoft.com>
1999-10-15 09:23:06 +0000

    Problem with '| grep -v' in iss-base 4.0 beta 2.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@792 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- test/all/hact/test_geyacc.sh
+++ test/all/hact/test_geyacc.sh
@@ -4,12 +4,14 @@ mkdir geyacc
 cd geyacc
 if [ "$1" = "-finalize" ]; then
 	cp $GOBO/src/geyacc/hact.ace Ace.ace
-	ibcomp -executable -finalize -new -ace Ace.ace -project toto.eif | grep -v "\(Degree\|Generating global files\|Features marked\|You must now run\|toto_gen[\\/]F_code$\)"
+	ibcomp -executable -finalize -new -ace Ace.ace -project toto.eif > tmp10.txt
+	cat tmp10.txt | grep -v "\(Degree\|Generating global files\|Features marked\|You must now run\|toto_gen[\\/]F_code$\)"
 	cd toto_gen/F_code
 	fish > tmp11.txt 2>&1
 else
 	sed "s/--assertion (all)/assertion (all)/g" $GOBO/src/geyacc/hact.ace > Ace.ace
-	ibcomp -executable -new -ace Ace.ace -project toto.eif | grep -v "\(Degree\|Generating global files\|You must now run\|toto_gen[\\/]W_code$\)"
+	ibcomp -executable -new -ace Ace.ace -project toto.eif > tmp10.txt
+	cat tmp10.txt | grep -v "\(Degree\|Generating global files\|You must now run\|toto_gen[\\/]W_code$\)"
 	cd toto_gen/W_code
 	fish > tmp11.txt 2>&1
 fi
--- test/all/hact/test_structure.sh
+++ test/all/hact/test_structure.sh
@@ -14,12 +14,14 @@ for dir in `ls -d $GOBO_UNIX/test/structure/test*` ; do
 
 	if [ "$1" = "-finalize" ]; then
 		cp $dir/hact.ace Ace.ace
-		ibcomp -executable -finalize -new -ace Ace.ace -project toto.eif | grep -v "\(Degree\|Generating global files\|Features marked\|You must now run\|toto_gen[\\/]F_code$\)"
+		ibcomp -executable -finalize -new -ace Ace.ace -project toto.eif > tmp10.txt
+		cat tmp10.txt | grep -v "\(Degree\|Generating global files\|Features marked\|You must now run\|toto_gen[\\/]F_code$\)"
 		cd toto_gen/F_code
 		fish > tmp11.txt 2>&1
 	else
 		sed "s/--assertion (all)/assertion (all)/g" $dir/hact.ace > Ace.ace
-		ibcomp -executable -new -ace Ace.ace -project toto.eif | grep -v "\(Degree\|Generating global files\|You must now run\|toto_gen[\\/]W_code$\)"
+		ibcomp -executable -new -ace Ace.ace -project toto.eif > tmp10.txt
+		cat tmp10.txt | grep -v "\(Degree\|Generating global files\|You must now run\|toto_gen[\\/]W_code$\)"
 		cd toto_gen/W_code
 		fish > tmp11.txt 2>&1
 	fi
