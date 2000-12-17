74bd0f9bc2ec351366ee11192951d1e4817fc80c
Eric Bezault <ericb@gobosoft.com>
2000-12-17 13:45:29 +0000

    Fixed problem under Linux: filenames are case-sensitive.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@964 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- test/all/common/test_gepp.sh
+++ test/all/common/test_gepp.sh
@@ -8,13 +8,14 @@ mkdir spec/se
 GOBO_UNIX=`echo $GOBO | sed "s/\\\\\\/\//g"`
 export GOBO_UNIX
 for file in `ls $GOBO_UNIX/library/kernel/*.ge` ; do
-	for compiler in ISE HACT VE SE ; do
-		./gepp -D$compiler $file spec/$compiler/`basename $file .ge`.e > tmp1.txt 2>&1
-		if [ -s tmp1.txt ]; then
-			echo "./gepp -D$compiler $file spec/$compiler/`basename $file .ge`.e"
-			cat tmp1.txt
-		fi
-	done
+	./gepp -DISE $file spec/ise/`basename $file .ge`.e > tmp1.txt
+	./gepp -DHACT $file spec/hact/`basename $file .ge`.e >> tmp1.txt
+	./gepp -DSE $file spec/se/`basename $file .ge`.e >> tmp1.txt
+	./gepp -DVE $file spec/ve/`basename $file .ge`.e >> tmp1.txt
+	if [ -s tmp1.txt ]; then
+		echo "./gepp -D$compiler $file spec/$compiler/`basename $file .ge`.e"
+		cat tmp1.txt
+	fi
 done
 diff -r $GOBO/library/kernel/spec spec > tmp2.txt 2>&1
 if [ -s tmp2.txt ]; then
