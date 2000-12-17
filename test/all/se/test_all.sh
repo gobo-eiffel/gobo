7195be38f687da50a15613cafd310071f46d9232
Eric Bezault <ericb@gobosoft.com>
2000-12-17 13:46:19 +0000

    Library classes are tested with 'getest'.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@965 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- test/all/se/test_all.sh
+++ test/all/se/test_all.sh
@@ -7,11 +7,11 @@ $GOBO/test/all/se/test_precomp.sh $1
 
 echo ""
 echo "Testing Gobo Eiffel Structure Library..."
-$GOBO/test/all/se/test_structure.sh $1
+#$GOBO/test/all/se/test_structure.sh $1
 
 echo ""
 echo "Testing Gobo Eiffel Time Library..."
-$GOBO/test/all/se/test_time.sh $1
+#$GOBO/test/all/se/test_time.sh $1
 
 echo ""
 $GOBO/test/all/common/test_all.sh $1 se
