d5d361b0f9ef6fbe1e3ed2c735cf91971bdf10e9
Eric Bezault <ericb@gobosoft.com>
2000-12-17 13:48:06 +0000

    Added -o option.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@967 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- src/gelex/se.sh
+++ src/gelex/se.sh
@@ -9,8 +9,9 @@
 # revision:   "$Revision$"
 
 
-echo ${GOBO}/src/gelex/>				loadpath.se
-echo ${GOBO}/library/loadpath.se>>		loadpath.se
+if [ ! -f loadpath.se ]; then
+	cp $GOBO/src/gelex/loadpath.se .
+fi
 
 export geoptions="-boost -no_split -no_style_warning -no_gc"
-compile $geoptions GELEX execute
+compile $geoptions -o gelex GELEX execute
--- src/getest/se.sh
+++ src/getest/se.sh
@@ -3,14 +3,15 @@
 # system:     "Gobo Eiffel Test"
 # compiler:   "SmallEiffel -0.78"
 # author:     "Eric Bezault <ericb@gobosoft.com>"
-# copyright:  "Copyright (c) 1999, Eric Bezault and others"
+# copyright:  "Copyright (c) 2000, Eric Bezault and others"
 # license:    "Eiffel Forum Freeware License v1 (see forum.txt)"
 # date:       "$Date$"
 # revision:   "$Revision$"
 
 
-echo ${GOBO}/src/getest/>					loadpath.se
-echo ${GOBO}/library/loadpath.se>>			loadpath.se
+if [ ! -f loadpath.se ]; then
+	cp $GOBO/src/getest/loadpath.se .
+fi
 
 export geoptions="-boost -no_split -no_style_warning -no_gc"
-compile $geoptions GETEST execute
+compile $geoptions -o getest GETEST execute
