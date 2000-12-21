7e916028cea37855d827435718a3fee32291d004
Eric Bezault <ericb@gobosoft.com>
2000-12-21 22:23:55 +0000

    under development.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@990 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- work/script/make_develop.sh
+++ work/script/make_develop.sh
@@ -2,23 +2,37 @@
 
 # usage: make_develop <platform> <binaries>
 
+
+# Checkout source code from CVS repository.
 cvs checkout gobo
 
+# Get binaries to allow bootstraping.
 cp $2/* gobo/bin
-cd gobo
 
+# Set environment variable $GOBO.
+cd gobo
 GOBO=`pwd`
 export GOBO
 
-cat $GOBO/misc/platform.make | \
-sed "s/^GOBO_PLATFORM/#GOBO_PLATFORM/g" | \
-sed "s/^#GOBO_PLATFORM=$1/GOBO_PLATFORM=$1/g" > $GOBO/misc/platform.make
+# Generate platform-dependent Makefile template.
+cd $GOBO/misc
+sed "s/^GOBO_PLATFORM/#GOBO_PLATFORM/g" platform.make \
+sed "s/^#GOBO_PLATFORM=$1/GOBO_PLATFORM=$1/g" > platform.tmp
+mv platform.tmp platform.make
 
+# Generate files in $GOBO/library.
 cd $GOBO/library/kernel
 make depend spec
+cd $GOBO/library/lexical/lex
+make parser scanner
+cd $GOBO/library/lexical/regexp
+make parser scanner
+cd $GOBO/library/parse/yacc
+make parser scanner
 cd $GOBO/library/utility
 make depend spec
 
+# Generate files in $GOBO/src.
 cd $GOBO/src/gelex
 make depend ise.ace hact.ace
 cd $GOBO/src/gelint
@@ -30,3 +44,37 @@ cd $GOBO/src/getest
 make depend ise.ace hact.ace
 cd $GOBO/src/geyacc
 make depend ise.ace hact.ace
+
+# Compile binaries with SmallEiffel compiler
+# and copy them to $GOBO/bin.
+cd $GOBO
+mkdir BINGEN
+cd BINGEN
+$GOBO/work/delivery/make_bin.sh $GOBO/bin
+cd $GOBO
+rm -rf BINGEN
+
+# Generate files in $GOBO/example.
+cd $GOBO/example/lexical/ascii
+make depend ise.ace hact.ace
+make scanner
+cd $GOBO/example/lexical/calculator
+make depend hact.ace
+make scanner
+cd $GOBO/example/lexical/eiffel
+make depend ise.ace hact.ace
+make scanner
+cd $GOBO/example/lexical/gegrep
+make depend ise.ace hact.ace
+cd $GOBO/example/parse/calc
+make depend ise.ace hact.ace
+make parser
+cd $GOBO/example/parse/eiffel
+make depend ise.ace hact.ace
+make parser scanner
+cd $GOBO/example/parse/mcalc
+make depend ise.ace hact.ace
+make parser
+cd $GOBO/example/parse/rpcalc
+make depend ise.ace hact.ace
+make parser
