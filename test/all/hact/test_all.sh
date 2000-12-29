d8650eb29cf06f10ad1a88f5deed9c2804cdbefc
Eric Bezault <ericb@gobosoft.com>
2000-12-29 11:11:51 +0000

    Removed obsolete tests.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@1014 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- test/all/hact/test_all.sh
+++ test/all/hact/test_all.sh
@@ -2,14 +2,7 @@
 
 # usage: test_all.sh [--finalize]
 
-echo "Testing Gobo Eiffel Structure Library..."
-$GOBO/test/all/hact/test_structure.sh $1
 
-echo ""
-echo "Testing Gobo Eiffel Time Library..."
-#$GOBO/test/all/hact/test_time.sh $1
-
-echo ""
 $GOBO/test/all/common/test_all.sh $1 hact
 
 echo ""
