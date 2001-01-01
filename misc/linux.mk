79fea26b3019916c4f3afc6899f2cffdccb12291
Eric Bezault <ericb@gobosoft.com>
2001-01-01 19:09:43 +0000

    Moved from linux.make
    git-svn-id: file:///c/gobo/svn/gobo/trunk@1026 fc6a2fa8-d027-0410-966b-9b90f175b7b2

---
+++ misc/linux.mk
@@ -0,0 +1,29 @@
+# system:     "Linux-dependent Makefile template"
+# author:     "Eric Bezault <ericb@gobosoft.com>"
+# copyright:  "Copyright (c) 2000, Eric Bezault and others"
+# license:    "Eiffel Forum Freeware License v1 (see forum.txt)"
+# date:       "$Date$"
+# revision:   "$Revision$"
+
+
+# Shell Commands
+SHELL=/bin/sh
+MAKE=make
+CP=cp
+RM=rm
+MKDIR=mkdir
+ECHO=echo
+SED=sed
+GEPP=${GOBO}/bin/gepp
+GELEX=${GOBO}/bin/gelex
+GEYACC=${GOBO}/bin/geyacc
+GETEST=${GOBO}/bin/getest
+
+# Filenames
+EXE=
+
+# SmallEiffel
+SE_PCFLAGS=
+
+# ISE Eiffel
+ISE_PFFLAGS=
