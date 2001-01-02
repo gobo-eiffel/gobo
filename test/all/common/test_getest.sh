865febb024c153bb7559c6216a0beba2eb869cdc
Eric Bezault <ericb@gobosoft.com>
2001-01-02 15:14:48 +0000

    Fixed problem with melted files.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@1046 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- test/all/common/test_getest.sh
+++ test/all/common/test_getest.sh
@@ -17,6 +17,12 @@ cd xtest2
 mkdir TESTGEN
 cp $GOBO/test/time/Makefile .
 cp $GOBO/test/time/getest.$compiler .
+if [ -f ../getest.melted ]; then
+	cp ../getest.melted .
+fi
+if [ -f ../getest.h2o ]; then
+	cp ../getest.h2o .
+fi
 ../getest --$compiler > tmp1.txt
 cd $root_dir
 diff xtest1/tmp1.txt xtest2/tmp1.txt
