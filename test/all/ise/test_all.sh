2a1da58de356a8b34cc053a70a07d685e82fc9c6
Eric Bezault <ericb@gobosoft.com>
2001-04-29 08:49:27 +0000

    Precompilation tests are now done in the Makefile.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@1312 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- test/all/ise/test_all.sh
+++ test/all/ise/test_all.sh
@@ -5,12 +5,5 @@
 GOBO=`echo $GOBO | sed "s/\\\\\\/\//g"`
 export GOBO
 
-echo "Testing precomp..."
-$GOBO/test/all/ise/test_precomp.sh $1
-
-echo ""
-echo "Testing precomp-base..."
-$GOBO/test/all/ise/test_precomp_base.sh $1
-
 cp $GOBO/test/all/ise/Makefile .
 make all$1
