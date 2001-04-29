5da4b48440702e4306d505450a01b529d5814c45
Eric Bezault <ericb@gobosoft.com>
2001-04-29 15:03:09 +0000

    Remove hidden files at the end of the script.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@1327 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- work/script/make_delivery.sh
+++ work/script/make_delivery.sh
@@ -15,17 +15,18 @@ GOBO=`echo $GOBO | sed "s/^\/\/\([a-zA-Z]\)\//\1:\//g"`
 export GOBO
 echo ''; echo "#### \$GOBO = $GOBO"
 
-# Remove hidden files.
-rm -rf $GOBO/work
-rm -rf $GOBO/doc/structure/visio
-rm -rf $GOBO/doc/time/visio
-
 # Get binaries to allow bootstraping.
+cd $rootdir
 cd $2
 cp * $GOBO/bin
 cd $rootdir
 
 # Build Gobo files.
 $GOBO/work/script/make_gobo.sh $1
+
+# Remove hidden files.
+rm -rf $GOBO/work
+rm -rf $GOBO/doc/structure/visio
+rm -rf $GOBO/doc/time/visio
 
 cd $rootdir
