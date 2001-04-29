c89dbb57dd1c10de7c676b79272215ba534ac2ea
Eric Bezault <ericb@gobosoft.com>
2001-04-29 12:22:16 +0000

    getest configuration files have already been copied in the current directory make the enclosing Makefile.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@1316 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- test/all/common/test_concat1.sh
+++ test/all/common/test_concat1.sh
@@ -5,6 +5,5 @@
 compiler=$1
 root_dir=`pwd`
 
-cp $GOBO/example/test/concat1/getest.$compiler .
 $GOBO/bin/getest --$compiler > tmp1.txt 2>&1
 diff tmp1.txt $GOBO/test/all/common/concat1.log
--- test/all/common/test_concat2.sh
+++ test/all/common/test_concat2.sh
@@ -5,6 +5,5 @@
 compiler=$1
 root_dir=`pwd`
 
-cp $GOBO/example/test/concat2/getest.$compiler .
 $GOBO/bin/getest --$compiler > tmp1.txt 2>&1
 diff tmp1.txt $GOBO/test/all/common/concat2.log
