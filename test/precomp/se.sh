5ce4a553f26f09f88aca7b8fa0f67f3ee5ece7aa
Eric Bezault <ericb@gobosoft.com>
2001-04-29 08:46:16 +0000

    Added new libraries to the precompilation.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@1308 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- test/precomp/se.sh
+++ test/precomp/se.sh
@@ -1,30 +1,27 @@
 #!/bin/sh
 
 # system:     "Gobo Eiffel Libraries precompilation"
-# compiler:   "SmallEiffel -0.76"
+# compiler:   "SmallEiffel"
 # author:     "Eric Bezault <ericb@gobosoft.com>"
-# copyright:  "Copyright (c) 1999-2000, Eric Bezault and others"
+# copyright:  "Copyright (c) 1999-2001, Eric Bezault and others"
 # license:    "Eiffel Forum Freeware License v1 (see forum.txt)"
 # date:       "$Date$"
 # revision:   "$Revision$"
 
-echo ${GOBO}/library/loadpath.se>			loadpath.se
-echo ${GOBO}/library/other/EiffelBase/>>	loadpath.se
-echo ${GOBO}/library/other/EiffelTime/>>	loadpath.se
-
 export geoptions="-no_style_warning"
 export geformat="-html2"
 
-GOBO_UNIX=`echo $GOBO | sed "s/\\\\\\/\//g"`
-export GOBO_UNIX
-find ${GOBO_UNIX}/library \
-	-path ${GOBO_UNIX}/library/kernel/spec -prune \
-	-path ${GOBO_UNIX}/library/time/spec -prune \
-	-path ${GOBO_UNIX}/library/utility/spec -prune \
+GOBO=`echo $GOBO | sed "s/\\\\\\/\//g"`
+export GOBO
+
+find ${GOBO}/library \
+	-path ${GOBO}/library/kernel/spec -prune \
+	-path ${GOBO}/library/time/spec -prune \
+	-path ${GOBO}/library/utility/spec -prune \
 	-o -name *.e -exec short $geformat $geoptions {} \; > precomp.html
-find ${GOBO_UNIX}/library/kernel/spec/se \
+find ${GOBO}/library/kernel/spec/se \
 	-name *.e -exec short $geformat $geoptions {} \; >> precomp.html
-find ${GOBO_UNIX}/library/time/spec/se \
+find ${GOBO}/library/time/spec/se \
 	-name *.e -exec short $geformat $geoptions {} \; >> precomp.html
-find ${GOBO_UNIX}/library/utility/spec/se \
+find ${GOBO}/library/utility/spec/se \
 	-name *.e -exec short $geformat $geoptions {} \; >> precomp.html
