fa3b9af99b20a45629afb625361535d4e72ffa12
Eric Bezault <ericb@gobosoft.com>
2001-04-29 14:20:01 +0000

    Use 'make_delivery.sh' instead.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@1325 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- work/script/make_delivery.bat
+++
@@ -1,9 +0,0 @@
-cvs export -D now gobo
-cd gobo
-rm -rf work
-rm -rf doc\structure\visio
-copy %GOBO%\bin\gelex.exe bin
-copy %GOBO%\bin\geyacc.exe bin
-copy %GOBO%\bin\gepp.exe bin
-cd library\kernel
-call make_spec.bat
