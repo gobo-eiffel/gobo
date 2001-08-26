6b25ef8cf64b6e918c530329aeb668ebdefab1a1
Eric Bezault <ericb@gobosoft.com>
2001-08-26 14:23:43 +0000

    Fixed 'lcc' compilation flags.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@1593 fc6a2fa8-d027-0410-966b-9b90f175b7b2

--- work/bootstrap/bootstrap.bat
+++ work/bootstrap/bootstrap.bat
@@ -6,82 +6,105 @@ rem usage: bootstrap.bat <c_compiler> <eiffel_compiler>
 set CC=%1
 set EIF=%2
 set CP=copy
-set DEL=del
+set RM=del
 set BIN_DIR=%GOBO%\bin
 set OBJ=.obj
 set EXE=.exe
 
 cd %GOBO%\work\bootstrap
 
+if .%CC%. == .. goto usage
+if .%CC%. == .-help. goto usage
+if .%CC%. == .-h. goto usage
+if .%CC%. == .-?. goto usage
+if .%CC%. == ./h. goto usage
+if .%CC%. == ./?. goto usage
+if .%EIF%. == .. goto usage
+
 if .%CC%. == .cl. goto cl
 if .%CC%. == .bcc32. goto bcc32
-if .%CC%. == .lcc-win32. goto lcc-win32
+if .%CC%. == .lcc. goto lcc
 if .%CC%. == .gcc. goto gcc
 if .%CC%. == .no_c. goto install
 echo Unknown C compiler: %CC%
-exit
+goto exit
 
 :cl
 	set CFLAGS=-O2 -nologo -D"WIN32" -w
-	%CC% %CFLAGS% -o %BIN_DIR%\gexace%EXE% gexace.c
-	%DEL% gexace%OBJ%
-	%CC% %CFLAGS% -o %BIN_DIR%\geant%EXE% geant.c
-	%DEL% geant%OBJ%
-	%CC% %CFLAGS% -o %BIN_DIR%\gelex%EXE% gelex.c
-	%DEL% gelex%OBJ%
-	%CC% %CFLAGS% -o %BIN_DIR%\geyacc%EXE% geyacc.c
-	%DEL% geyacc%OBJ%
-	%CC% %CFLAGS% -o %BIN_DIR%\gepp%EXE% gepp.c
-	%DEL% gepp%OBJ%
+	%CC% %CFLAGS% -o%BIN_DIR%\gexace%EXE% gexace.c
+	%RM% gexace%OBJ%
+	%CC% %CFLAGS% -o%BIN_DIR%\geant%EXE% geant.c
+	%RM% geant%OBJ%
+	%CC% %CFLAGS% -o%BIN_DIR%\gelex%EXE% gelex.c
+	%RM% gelex%OBJ%
+	%CC% %CFLAGS% -o%BIN_DIR%\geyacc%EXE% geyacc.c
+	%RM% geyacc%OBJ%
+	%CC% %CFLAGS% -o%BIN_DIR%\gepp%EXE% gepp.c
+	%RM% gepp%OBJ%
 	goto install
 
 :bcc32
 	set CFLAGS=-5 -w-aus -w-par -w-rvl -O2 -O-v
 	%CC% %CFLAGS% -ogexace%EXE% gexace.c
 	%CP% gexace%EXE% %BIN_DIR%
-	%DEL% gexace%EXE% gexace.tds
+	%RM% gexace%EXE% gexace.tds
 	%CC% %CFLAGS% -ogeant%EXE% geant.c
 	%CP% geant%EXE% %BIN_DIR%
-	%DEL% geant%EXE% geant.tds
+	%RM% geant%EXE% geant.tds
 	%CC% %CFLAGS% -ogelex%EXE% gelex.c
 	%CP% gelex%EXE% %BIN_DIR%
-	%DEL% gelex%EXE% gelex.tds
+	%RM% gelex%EXE% gelex.tds
 	%CC% %CFLAGS% -ogeyacc%EXE% geyacc.c
 	%CP% geyacc%EXE% %BIN_DIR%
-	%DEL% geyacc%EXE% geyacc.tds
+	%RM% geyacc%EXE% geyacc.tds
 	%CC% %CFLAGS% -ogepp%EXE% gepp.c
 	%CP% gepp%EXE% %BIN_DIR%
-	%DEL% gepp%EXE% gepp.tds
+	%RM% gepp%EXE% gepp.tds
 	goto install
 
-:lcc-win32
+:lcc
 	set CFLAGS=-O
-	%CC% %CFLAGS% -o %BIN_DIR%\gexace%EXE% gexace.c
-	%DEL% gexace%OBJ%
-	%CC% %CFLAGS% -o %BIN_DIR%\geant%EXE% geant.c
-	%DEL% geant%OBJ%
-	%CC% %CFLAGS% -o %BIN_DIR%\gelex%EXE% gelex.c
-	%DEL% gelex%OBJ%
-	%CC% %CFLAGS% -o %BIN_DIR%\geyacc%EXE% geyacc.c
-	%DEL% geyacc%OBJ%
-	%CC% %CFLAGS% -o %BIN_DIR%\gepp%EXE% gepp.c
-	%DEL% gepp%OBJ%
+	set LNK=lcclnk
+	set LNKFLAGS=-s
+	%CC% %CFLAGS% gexace.c
+	%LNK% %LNKFLAGS% -o %BIN_DIR%\gexace%EXE% gexace%OBJ%
+	%RM% gexace%OBJ%
+	%CC% %CFLAGS% geant.c
+	%LNK% %LNKFLAGS% -o %BIN_DIR%\geant%EXE% geant%OBJ%
+	%RM% geant%OBJ%
+	%CC% %CFLAGS% gelex.c
+	%LNK% %LNKFLAGS% -o %BIN_DIR%\gelex%EXE% gelex%OBJ%
+	%RM% gelex%OBJ%
+	%CC% %CFLAGS% geyacc.c
+	%LNK% %LNKFLAGS% -o %BIN_DIR%\geyacc%EXE% geyacc%OBJ%
+	%RM% geyacc%OBJ%
+	%CC% %CFLAGS% gepp.c
+	%LNK% %LNKFLAGS% -o %BIN_DIR%\gepp%EXE% gepp%OBJ%
+	%RM% gepp%OBJ%
 	goto install
 
 :gcc
 	set CFLAGS=-O2
 	%CC% %CFLAGS% -o %BIN_DIR%\gexace%EXE% gexace.c
-	%DEL% gexace%OBJ%
+	%RM% gexace%OBJ%
 	%CC% %CFLAGS% -o %BIN_DIR%\geant%EXE% geant.c
-	%DEL% geant%OBJ%
+	%RM% geant%OBJ%
 	%CC% %CFLAGS% -o %BIN_DIR%\gelex%EXE% gelex.c
-	%DEL% gelex%OBJ%
+	%RM% gelex%OBJ%
 	%CC% %CFLAGS% -o %BIN_DIR%\geyacc%EXE% geyacc.c
-	%DEL% geyacc%OBJ%
+	%RM% geyacc%OBJ%
 	%CC% %CFLAGS% -o %BIN_DIR%\gepp%EXE% gepp.c
-	%DEL% gepp%OBJ%
+	%RM% gepp%OBJ%
 	goto install
 
 :install
+	goto exit
 
 :bootstrap
+	goto exit
+
+:usage
+	echo "usage: bootstrap.bat <c_compiler> <eiffel_compiler>"
+	goto exit
+  
+:exit
