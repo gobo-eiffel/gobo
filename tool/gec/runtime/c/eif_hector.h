c169025c95fd63c3fbc4202b4332cdcdc20148e9
Eric Bezault <ericb@gobosoft.com>
2009-08-27 14:24:03 +0000

    Interoperability with ISE.
    git-svn-id: file:///c/gobo/svn/gobo/trunk@6665 fc6a2fa8-d027-0410-966b-9b90f175b7b2

---
+++ tool/gec/runtime/c/eif_hector.h
@@ -0,0 +1,16 @@
+/*
+	description:
+
+		"C declarations for the ISE Eiffel runtime."
+
+	system: "Gobo Eiffel Compiler"
+	copyright: "Copyright (c) 2009, Eric Bezault and others"
+	license: "MIT License"
+	date: "$Date$"
+	revision: "$Revision$"
+*/
+
+#ifndef EIF_HECTOR_H
+#define EIF_HECTOR_H
+
+#endif
