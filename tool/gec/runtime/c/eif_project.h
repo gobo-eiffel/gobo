be0011108d495f36e0be66466b607978e475dc32
Eric Bezault <ericb@gobosoft.com>
2013-07-28 05:53:26 -0700

    Made it possible to compile some ISE libraries.

---
+++ tool/gec/runtime/c/eif_project.h
@@ -0,0 +1,26 @@
+/*
+	description:
+
+		"Part of ISE Eiffel runtime."
+
+	system: "Gobo Eiffel Compiler"
+	copyright: "Copyright (c) 2013, Eric Bezault and others"
+	license: "MIT License"
+	date: "$Date$"
+	revision: "$Revision$"
+*/
+
+#ifndef EIF_PROJECT_H
+#define EIF_PROJECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+EIF_INTEGER egc_prof_enabled;	  /* Is the Eiffel profiler on */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
