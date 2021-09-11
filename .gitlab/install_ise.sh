ac0b474528a23a0d6b05ada1fd78d59bf7d4d4d6
Eric Bezault <ericb@gobosoft.com>
2021-09-12 00:03:49 +0200

    Allow tests to be executed with ISE Eiffel from a scheduled GitLab pipeline.

---
+++ .gitlab/install_ise.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install ISE Eiffel (see https://www.eiffel.org/setup/)
+
+cd $GOBO/..
+export GITLAB_ISE_PLATFORM=linux-x86-64
+export GITLAB_ISE_VERSION=20.05_rev_104521
+curl -sSL https://www.eiffel.org/setup/install.sh | bash -s -- --url https://ftp.eiffel.com/pub/beta/nightly/Eiffel_$GITLAB_ISE_VERSION-$GITLAB_ISE_PLATFORM.tar.bz2 > eiffel.rc
+source ./eiffel.rc
+# Make sure that we are using our version of the Gobo tools.
+rm -rf $ISE_EIFFEL/library/gobo/spec
+echo `ec -version`
+cd $GOBO
