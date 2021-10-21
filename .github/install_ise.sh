77979e9ff605860bb770e3c8355405821dad96e6
Eric Bezault <ericb@gobosoft.com>
2021-10-22 01:22:38 +0200

    Fixed MacOS pipeline.

--- .github/install_ise.sh
+++ .github/install_ise.sh
@@ -6,9 +6,12 @@ export GITHUB_ISE_VERSION=20.05_rev_104521
 if [ "$RUNNER_OS" = "Linux" ]; then export GITHUB_ISE_PLATFORM=linux-x86-64; fi
 if [ "$RUNNER_OS" = "macOS" ]; then export GITHUB_ISE_PLATFORM=macosx-x86-64; fi
 
+if [ "$RUNNER_OS" = "Linux" ]; then
+    sudo apt-get update
+    sudo apt-get -y install libgtk2.0-0
+fi
+
 cd $GOBO
-sudo apt-get update
-sudo apt-get -y install libgtk2.0-0
 curl -sSL https://www.eiffel.org/setup/install.sh | bash -s -- --url https://ftp.eiffel.com/pub/beta/nightly/Eiffel_$GITHUB_ISE_VERSION-$GITHUB_ISE_PLATFORM.tar.bz2 > eiffel.rc
 source ./eiffel.rc
 # Make sure that we are using our version of the Gobo tools.
