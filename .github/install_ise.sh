a5358e015af749176ff26e394e38a8044df2d159
Eric Bezault <ericb@gobosoft.com>
2021-10-21 21:11:39 +0200

    Make sure that GTK 2.0 is installed when running ISE Eiffel.

--- .github/install_ise.sh
+++ .github/install_ise.sh
@@ -6,6 +6,8 @@ export GITHUB_ISE_VERSION=20.05_rev_104521
 export GITHUB_ISE_PLATFORM=linux-x86-64
 
 cd $GOBO
+apt-get update
+apt-get -y install libgtk2.0-0
 curl -sSL https://www.eiffel.org/setup/install.sh | bash -s -- --url https://ftp.eiffel.com/pub/beta/nightly/Eiffel_$GITHUB_ISE_VERSION-$GITHUB_ISE_PLATFORM.tar.bz2 > eiffel.rc
 source ./eiffel.rc
 # Make sure that we are using our version of the Gobo tools.
