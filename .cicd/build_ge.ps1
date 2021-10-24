f4a2c018a2ab0905d3b004aeb45e44f0bbe4f9cb
Eric Bezault <ericb@gobosoft.com>
2021-10-25 00:07:16 +0200

    Investigating why the script does not work.

--- .cicd/build_ge.ps1
+++ .cicd/build_ge.ps1
@@ -17,36 +17,36 @@
 	License: "MIT License"
 #>
 
-param
-(
-	[Parameter(Mandatory=$true)]
-	[ValidateSet('github', 'gitlab', 'travis')] 
-	[string]
-	$CiTool
-)
+#param
+#(
+#	[Parameter(Mandatory=$true)]
+#	[ValidateSet('github', 'gitlab', 'travis')] 
+#	[string]
+#	$CiTool
+#)
 
-function Invoke-Environment {
-	param
-	(
-		[Parameter(Mandatory=$true)] [string]
-		$Command
-	)
+#function Invoke-Environment {
+#	param
+#	(
+#		[Parameter(Mandatory=$true)] [string]
+#		$Command
+#	)
+#
+#	cmd /c "$Command > nul 2>&1 && set" | .{process{
+#		if ($_ -match '^([^=]+)=(.*)') {
+#			[System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+#		}
+#	}}
+#}
 
-	cmd /c "$Command > nul 2>&1 && set" | .{process{
-		if ($_ -match '^([^=]+)=(.*)') {
-			[System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
-		}
-	}}
-}
-
-if ($CiTool == 'github') {
+#if ($CiTool == 'github') {
 #	$env:GOBO = $env:GITHUB_WORKSPACE
 #	Invoke-Environment("C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsx86_amd64.bat")
-} elseif ($CiTool == 'gitlab') {
-	$env:GOBO = $env:CI_PROJECT_DIR
-	Invoke-Environment("C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsx86_amd64.bat")
-}
+#} elseif ($CiTool == 'gitlab') {
+#	$env:GOBO = $env:CI_PROJECT_DIR
+#	Invoke-Environment("C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsx86_amd64.bat")
+#}
 
 #$env:PATH = "$env:GOBO/bin" + [IO.Path]::PathSeparator + $env:PATH
 #. $env:GOBO/bin/install.bat -v msc
-gec --version
+#gec --version
