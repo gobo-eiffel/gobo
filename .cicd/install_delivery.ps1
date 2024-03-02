0188c07a780bd6e67497ee0cb8866d3ac03a53b8
Eric Bezault <ericb@gobosoft.com>
2024-03-02 10:26:03 +0100

    cicd: test delivery.

---
+++ .cicd/install_delivery.ps1
@@ -0,0 +1,61 @@
+<#
+.SYNOPSIS
+	Install Gobo Eiffel delivery.
+
+.DESCRIPTION
+	Install Gobo Eiffel delivery.
+
+.PARAMETER CiTool
+	CI tool (azure, github, gitlab).
+
+.EXAMPLE
+	# Install Gobo Eiffel delivery from the GitHub Actions pipeline:
+	install_delivery.ps1 github
+
+.NOTES
+	Copyright: "Copyright (c) 2024, Eric Bezault and others"
+	License: "MIT License"
+#>
+
+param
+(
+	[Parameter(Mandatory=$true)]
+	[ValidateSet("azure", "github", "gitlab")] 
+	[string] $CiTool
+)
+
+$ErrorActionPreference = "Stop"
+
+. "$PSScriptRoot/before_script.ps1" $CiTool zig
+if ($LastExitCode -ne 0) {
+	Write-Error "Command 'before_script.ps1 $CiTool zig' exited with code $LastExitCode"
+	exit $LastExitCode
+}
+
+$GOBO_CI_GE_ARCHIVE_FILENAME =  Get-ChildItem "gobo-*" -Name 
+
+if ($GOBO_CI_OS -eq "windows") {
+	Install-Module -Name 7Zip4PowerShell
+	Expand-7Zip -ArchiveFileName "$env:GOBO/$GOBO_CI_GE_ARCHIVE_FILENAME" -TargetPath "$env:GOBO"
+} else {
+	tar -xJf "$env:GOBO/$GOBO_CI_GE_ARCHIVE_FILENAME"
+	if ($LastExitCode -ne 0) {
+		Write-Error "Command 'tar -xJf $env:GOBO/$GOBO_CI_GE_ARCHIVE_FILENAME' exited with code $LastExitCode"
+		exit $LastExitCode
+	}
+}
+Remove-Item "$env:GOBO/$GOBO_CI_GE_ARCHIVE_FILENAME"
+
+Remove-Item "$env:GOBO/bin" -Recurse -Force
+Move-Item -Path "$env:GOBO/gobo/bin" -Destination "$env:GOBO"
+Remove-Item "$env:GOBO/library" -Recurse -Force
+Move-Item -Path "$env:GOBO/gobo/library" -Destination "$env:GOBO"
+Remove-Item "$env:GOBO/tool" -Recurse -Force
+Move-Item -Path "$env:GOBO/gobo/tool" -Destination "$env:GOBO"
+Remove-Item "$env:GOBO/gobo" -Recurse -Force
+
+gec --version --verbose
+if ($LastExitCode -ne 0) {
+	Write-Error "Command 'gec --version --verbose' exited with code $LastExitCode"
+	exit $LastExitCode
+}
---
+++ .cicd/test_delivery.ps1
@@ -0,0 +1,66 @@
+<#
+.SYNOPSIS
+	Test Gobo Eiffel delivery.
+
+.DESCRIPTION
+	Test Gobo Eiffel delivery.
+
+.PARAMETER CiTool
+	CI tool (azure, github, gitlab).
+
+.PARAMETER CCompiler
+	C Compiler (zig, gcc, clang, msc).
+
+.PARAMETER EiffelCompiler
+	Eiffel compiler (ge, debug_ge, ise, debug_ise).
+
+.PARAMETER SystemUnderTest
+	System under test (library, tool, etc.).
+
+.EXAMPLE
+	# Test Gobo Eiffel libraries using the Gobo Eiffel compiler from the GitHub Actions pipeline:
+	test_delivery.ps1 github zig ge library
+
+.NOTES
+	Copyright: "Copyright (c) 2021-2024, Eric Bezault and others"
+	License: "MIT License"
+#>
+
+param
+(
+	[Parameter(Mandatory=$true)]
+	[ValidateSet("azure", "github", "gitlab")] 
+	[string] $CiTool,
+	[Parameter(Mandatory=$true)]
+	[ValidateSet("zig", "gcc", "clang", "msc")] 
+	[string] $CCompiler,
+	[Parameter(Mandatory=$true)]
+	[ValidateSet("ge", "debug_ge", "ise", "debug_ise")] 
+	[string] $EiffelCompiler,
+	[Parameter(Mandatory=$true)]
+	[ValidateSet("library", "tool", "library/xslt", "tool/gec")] 
+	[string] $SystemUnderTest
+)
+
+$ErrorActionPreference = "Stop"
+
+. "$PSScriptRoot/install_delivery.ps1" $CiTool $CCompiler
+if ($LastExitCode -ne 0) {
+	Write-Error "Command 'install_delivery.ps1 $CiTool $CCompiler' exited with code $LastExitCode"
+	exit $LastExitCode
+}
+
+if ($EiffelCompiler.EndsWith("ise")) {
+	& "$PSScriptRoot/install_ise.ps1" $CiTool $CCompiler
+	if ($LastExitCode -ne 0) {
+		Write-Error "Command 'install_ise.ps1 $CiTool $CCompiler' exited with code $LastExitCode"
+		exit $LastExitCode
+	}
+}
+
+Set-Location "$env:GOBO/$SystemUnderTest"
+geant test_$EiffelCompiler
+if ($LastExitCode -ne 0) {
+	Write-Error "Command 'geant test_$EiffelCompiler' exited with code $LastExitCode"
+	exit $LastExitCode
+}
