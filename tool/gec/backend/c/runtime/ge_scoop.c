687f24d99a8632064780a71096c9ff5684fffd95
Eric Bezault <ericb@gobosoft.com>
2026-09-17 01:19:01 +0200

    scoop: added information for debugging.

--- tool/gec/backend/c/runtime/ge_scoop.c
+++ tool/gec/backend/c/runtime/ge_scoop.c
@@ -1231,8 +1231,8 @@ void GE_scoop_session_close(GE_scoop_region* a_caller, GE_scoop_session* a_sessi
 				l_to_be_freed = '\1';
 			} else if (l_callee->is_passive) {
 				/* A passive region as no associated processor. Its sessions are synchronously 
-				 * executed by the processor of their callers. So the caller's processor has
-				 * to do the house cleaning (e.g. call to `GE_remove_scoop_session`).
+				 * executed by the processor of their callers. So the the thread handling passive
+				 * regions has to do the house cleaning (e.g. call to `GE_remove_scoop_session`).
 				 */
 				GE_mutex_unlock((EIF_POINTER)a_session->mutex);
 				l_mutex_unlocked = '\1';
--- tool/gecop/validation/semantics/m9se/test_passive_region_2/bb.e
+++ tool/gecop/validation/semantics/m9se/test_passive_region_2/bb.e
@@ -15,12 +15,15 @@ feature
 			c: separate CC
 		do
 			create <NONE> c.make
+			print ("Calling BB.x%N")
 			x (c)
 		end
 
 	x (c: separate CC)
 		do
+			print ("BB.x%N")
 			c.h
+			print ("Failed")
 		end
 
 end
